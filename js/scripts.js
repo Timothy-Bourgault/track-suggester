b301f09adc854c8c17b75ab32d2308f92629f5d5
Tim Bourgault <tim.bourgault@gmail.com>
2016-08-12 13:14:49 -0700

    update HTML to include all values associated with the form, update images for form return value.

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+  $("form#aboutYou").submit(function(event) {
+
+    event.preventDefault();
+
+    $(".result").hide();
+
+
+    //var yrName = $("#yourName").val();
+    //var yrTentacles = parseInt($("#myTentacles").val());
+    //var yrElement = parseInt($("#myElement").val());
+    //var yrVaca = $("input:radio[name=myVacation]:checked").val();
+    //var yrSauce = $("input:radio[name=mySauce]:checked").val();
+    //var yrDomSub = parseInt($("#myDomSub").val());
+
+    //if (yrDomSub === 1) {
+      //$("#elderGod4").show();
+    //}
+    //else if (yrTentacles >= 2 && yrSauce === "Boolean Slime") {
+      //$("#elderGod1").show();
+    //}
+    //else if ((yrElement === "Fire" || yrElement === "Air") && yrVaca === "Nameless City") {
+      //$("#elderGod2").show();
+    //}
+    //else if (yrTentacles < 2 || yrElement === "Water" || yrVaca === "Rlyeh" || yrDomSub === 0) {
+      //$("#elderGod3").show();
+    //}
+    //else {
+      //$("#elderGod4").show();
+    //}
+  //});
+
+
+});
