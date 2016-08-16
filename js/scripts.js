2df9febf5d529e0df4027edfe9fe175b9234b996
Tim Bourgault <tim.bourgault@gmail.com>
2016-08-16 01:53:34 -0700

    fix issue with form appearing by simply creating an additional html page and linking to that through the button instead of using a modal window or hiding and showing text on index.html page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,41 @@
 $(document).ready(function() {
-  $("#work").click(function() {
+//debugger;
+  $("#work").click(function(event) {
     $('.show-onclick').show();
-
-    $("form#aboutYou").submit(function(event) {
+    $("#aboutYou").submit(function(event) {
       event.preventDefault();
-    })
+      $(".result").hide();
 
-    $(".result").hide();
+      var yrName = $("#yourName").val();
+      var yrFrontBack = parseInt($("#myFrontBack").val());
+      var yrEnvironment = parseInt($("#myEnvironment").val());
+      var yrStrengths = parseInt($("#myStrengths").val());
+      var yrSelfWorth = $("input:radio[name=mySelfWorth]:checked").val();
+      var yrDescriber = $("input:radio[name=myDescriber]:checked").val();
 
-    var yrName = $("#yourName").val();
-    var yrFrontBack = parseInt($("#myFrontBack").val());
-    var yrEnvironment = parseInt($("#myEnvironment").val());
-    var yrStrengths = parseInt($("#myStrengths").val());
-    var yrSelfWorth = $("input:radio[name=mySelfWorth]:checked").val();
-    var yrDescriber = $("input:radio[name=myDescriber]:checked").val();
+      if (yrFrontBack === 0) {
+        $("#designTrack").show();
+      }
 
-    if (yrFrontBack === 0) {
-      $("#designTrack").show();
-    }
+      else if (yrFrontBack == 1 && yrEnvironment == 1) {
+        $("#c-sharpTrack").show();
+      }
 
-    else if (yrFrontBack == 1 && yrEnvironment == 1) {
-      $("#c-sharpTrack").show();
-    }
+      else if (yrFrontBack == 1 && yrEnvironment == 2 && yrDescriber === "balance") {
+        $("#javaTrack").show();
+      }
 
-    else if (yrFrontBack == 1 && yrEnvironment == 2 && yrDescriber === "balance") {
-      $("#javaTrack").show();
-    }
+      else if (yrFrontBack == 1 && yrEnvironment == 0 && yrStrengths == 0 && yrSelfWorth === "Blunts") {
+        $("#noGo").show();
+      }
 
-    else if (yrFrontBack == 1 && yrEnvironment == 0 && yrStrengths == 0 && yrSelfWorth === "Blunts") {
-      $("#noGo").show();
-    }
+      else if (yrFrontBack == 1 && yrElement === "Water" || yrEnvironment === "Beach") {
+        $("#rubyTrack").show();
+      }
+      else if (yrFrontBack == 1 && yrElement === "Water" || yrEnvironment === "Beach") {
+        $("#phpTrack").show();
+      }
 
-    else if (yrFrontBack == 1 && yrElement === "Water" || yrEnvironment === "Beach") {
-      $("#rubyTrack").show();
-    }
-    else if (yrFrontBack == 1 && yrElement === "Water" || yrEnvironment === "Beach") {
-      $("#phpTrack").show();
-    }
+    });
   });
-
 });
